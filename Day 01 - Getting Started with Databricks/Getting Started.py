033ed01ad2fdfa98d43be4f56b265afe66d27bf3
saumyagupta2025 <saumyagupta2025>
2024-01-09 09:27:13 +0000

    Commit from Databricks

---
+++ Day 01 - Getting Started with Databricks/Getting Started.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+print("run python")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "run sql"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###this is markdown
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("Run Scala")
+
+# COMMAND ----------
+
+
